4b647c2ae7ecd176f9c370d1eec945e93525521f
kapsner <lorenz.kapsner@web.de>
2023-04-16 09:03:12 +0200

    chore: first commit

---
+++ r2source/wkn.R
@@ -0,0 +1,20 @@
+list1 <- list(
+  "VOW3.DE" = "Volkswagen Vz"
+  , "S9I.DE" = "Stemmer Imaging AG"
+  , "8TRA.DE" = "Traton SE"
+  , "MBG.DE" = "Mercedes-Benz Group AG"
+)
+
+list2 <- list(
+  "IMR.MU" = "AC Immune"
+  , "PAH3.DE" = "Porsche Automobil Holding SE"
+  , "DBXJ.DE" = "Xtrackers MSCI Japan UCITS ETF"
+  , "XDWD.DE" = "Xtrackers MSCI World UCITS ETF"
+  , "XMAW.DE" = "Xtrackers MSCI AC World ESG Screened UCITS ETF"
+  , "IVV" = "iShares Core S&P 500 ETF"
+  , "XSX6.DE" = "Xtrackers Stoxx Europe 600 UCITS ETF"
+  , "DXSE.DE" = "Xtrackers MSCI Europe Health Care ESG Screened UCITS ETF"
+  , "ESIH.DE" = "iShares MSCI Europe Health Care Sector UCITS ETF"
+  , "XZEU.DE" = "Xtrackers MSCI Europe ESG UCITS ETF"
+  , "ADA-EUR" = "Cardano EUR"
+)
