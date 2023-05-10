1c11687420e354e65882c72fc5a85018648827c8
kapsner <lorenz.kapsner@web.de>
2023-05-10 08:22:30 +0200

    Update wkn.R

--- r2source/wkn.R
+++ r2source/wkn.R
@@ -1,6 +1,5 @@
 list1 <- list(
-  "MBG.DE" = "Mercedes-Benz Group AG"
-  , "ELG.DE" = "Elmos Semiconductor SE"
+  "ELG.DE" = "Elmos Semiconductor SE"
   , "HEI.DE" = "HeidelbergCement AG"
   , "VOL4.F" = "AB Volvo (publ)"
   , "NJB.F" = "NIBE Industrier AB (publ)"
@@ -21,6 +20,7 @@ list2 <- list(
   , "ADA-EUR" = "Cardano EUR"
   , "VOW3.DE" = "Volkswagen Vz"
   , "8TRA.DE" = "Traton SE"
+  , "MBG.DE" = "Mercedes-Benz Group AG"
   , "INRG.MI" = "iShares Global Clean Energy UCITS ETF USD (Dist)"
   , "DX2J.SG" = "Xtrackers MSCI Europe Small Cap UCITS ETF"
   , "EXS2.DE" = "iShares TecDAX UCITS ETF (DE)"
