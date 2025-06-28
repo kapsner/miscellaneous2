31c76abe36d69e7e1078fe9ea6051419679c0be7
kapsner <lorenz.kapsner@web.de>
2025-06-28 10:48:19 +0200

    Update ric.R

--- r2source/ric.R
+++ r2source/ric.R
@@ -12,6 +12,7 @@ list1 <- list(
   #, "P911.DE" = list("name" = "Dr. Ing. h.c. F. Porsche AG", "src" = "yahoo")
   #, "QBY.DE" = list("name" = "q.beyond AG", "src" = "yahoo")
   , "SIX3.DE" = list("name" = "Sixt SE", "src" = "yahoo")
+  , "FR.PA" = list("name" = "Valeo SE", "src" = "yahoo")
   , "VIB3.DE" = list("name" = "Villeroy & Boch AG", "src" = "yahoo")
   , "WCH.DE" = list("name" = "Wacker Chemie AG", "src" = "yahoo")
 )
